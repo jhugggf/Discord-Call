a088a995495820a37c478c22c0c25f04ea469087
jhugggf <mqynyasr@gmail.com>
2025-02-05 14:14:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-clientId: "", // id bot
-    guildId: "", // id server
-    token: "" //token bot
+clientId: "1336652186322735158", // id bot
+    guildId: "1328022761171583017", // id server
+    token: "MTMzNjY1MjE4NjMyMjczNTE1OA.GDUO8G.Cn-Kco1r8ziACzJJw3HU_mlBIsI9R-NQQb8KCw" //token bot
 }
