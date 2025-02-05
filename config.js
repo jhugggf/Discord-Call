2bb55661cccd54dcdae246416844aea3e7daa3f3
jhugggf <mqynyasr@gmail.com>
2025-02-05 14:05:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 clientId: "", // id bot
     guildId: "", // id server
-    token: "" //token bot
+    token: "MTMzNjY1MjE4NjMyMjczNTE1OA.GDUO8G.Cn-Kco1r8ziACzJJw3HU_mlBIsI9R-NQQb8KCw" //token bot
 }
